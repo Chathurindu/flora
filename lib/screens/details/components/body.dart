987da75e707c87dba43783d11cdc529c65272661
chathurindu <100wpck@gmail.com>
2021-06-14 15:20:27 +0530

    init

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flower_app/constants.dart';
+import 'package:flower_app/models/product.dart';
+
+import 'product_image.dart';
+
+class Body extends StatelessWidget {
+  final Product product;
+
+  const Body({Key key, this.product}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // it provide us total height and width
+    Size size = MediaQuery.of(context).size;
+    // it enable scrolling on small devices
+    return SafeArea(
+      bottom: false,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              decoration: BoxDecoration(
+                color: kBackgroundColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: Hero(
+                      tag: '${product.id}',
+                      child: ProductPoster(
+                        size: size,
+                        image: product.image,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+                    child: Text(
+                      product.description,
+                      style: TextStyle(color: kTextLightColor, fontSize: 17),
+                    ),
+                  ),
+                  SizedBox(height: kDefaultPadding),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
